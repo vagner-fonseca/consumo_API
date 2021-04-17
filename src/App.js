159e1e5b4b91a0a437b51d99e5a5bc5398f0f2e6
unknown <vagner.cfonseca@gmail.com>
2021-04-17 18:30:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState('');
+  
+  
+  const handleChange =() => {
+    axios
+    .get('https://api.generated.photos/api/v1/faces?api_key=9uC7JwoBp-69TxfjlSauqg&order_by=random')
+    .then(res =>{
+     
+    const uri = res.data.faces[0].urls[4][512];
+    uri && setImage(uri);
+    })
+    .catch(err => {
+      console.log(err.message);
+    })
+
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Gerador de faces</h1>
+      {image && <img src={image} alt='AI Face'/>}
+      <button type='button' onClick={handleChange}>
+        Proximo Rosto
+      </button>
     </div>
   );
 }
